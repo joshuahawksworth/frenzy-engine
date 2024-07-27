6e424ab2e02774c6f4d1ea0134661f42bbc2c90e
joshuahawksworth <joshuahawksworth@me.com>
2024-07-27 20:33:30 +0100

    Project init

---
+++ FrenzyEditor/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace FrenzyEditor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
